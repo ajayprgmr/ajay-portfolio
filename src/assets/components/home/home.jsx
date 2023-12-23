0904a540e3ed407a7549356e0f006e53dad35d70
Ajay Kushwaha <ak383834@gmail.com>
2023-12-23 09:03:22 +0530

    added new code

--- src/assets/components/home/home.jsx
+++ src/assets/components/home/home.jsx
@@ -1,8 +1,54 @@
 import './home.scss'
+import data from '../../data.js'
+const highlighted_works = data.highlighted_works
+const social_media = data.social_media
 function home() {
+  const handleClick = () => {
+    
+  }
   return (
     <div className='home' id='home'>
-      home
+      <div className='left'>
+        <div className='intro'>
+          <h4>Ajay Kushwaha</h4>
+          <h6>A Full Stack Developer & Problem Solver</h6>
+        </div>
+        <div className='aboutMe' onClick={handleClick}>
+          <h5 onClick={handleClick}>About Me</h5>
+        </div>
+        <div className='down_arrow' onClick={handleClick}>
+          <img src='src\assets\images\down.png' alt='About Me' />
+        </div>
+        <div className='social_media'>
+          {social_media.map((social) => {
+            return (
+              <div className='social' key={social.id}>
+                <a href={social.link}>
+                  <img src={social.image} alt={social.name} className={`colors${social.id}`} />
+                </a>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <div className='right'>
+        <h3>
+          {' '}
+          <span>Highlighted works</span>
+        </h3>
+        <div className='project_container'>
+          {highlighted_works.map((project) => {
+            return (
+              <div className='project' key={project.id}>
+                <a href={project.link}>
+                  <img src={project.image} alt={project.name} />
+                </a>
+                <h6>{project.project_name}</h6>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/assets/data.js
@@ -0,0 +1,104 @@
+const highlighted_works = [
+  {
+    id: 1,
+    image:
+      'https://user-images.githubusercontent.com/89273037/176277077-27cbc3eb-d116-4e80-81d0-037611eb34c2.gif',
+    project_name: 'Sorting Visualizer',
+    link: 'https://ajayprgmr.github.io/Sorting-Visualizer/',
+  },
+  {
+    id: 2,
+    image:
+      'https://user-images.githubusercontent.com/89273037/176277077-27cbc3eb-d116-4e80-81d0-037611eb34c2.gif',
+    project_name: 'Image-Compressor',
+    link: 'https://ajayprgmr.github.io/Sorting-Visualizer/',
+  },
+  {
+    id: 2,
+    image:
+      'https://user-images.githubusercontent.com/89273037/176277077-27cbc3eb-d116-4e80-81d0-037611eb34c2.gif',
+    project_name: 'Image-Compressor',
+    link: 'https://ajayprgmr.github.io/Sorting-Visualizer/',
+  },
+  {
+    id: 2,
+    image:
+      'https://user-images.githubusercontent.com/89273037/176277077-27cbc3eb-d116-4e80-81d0-037611eb34c2.gif',
+    project_name: 'Image-Compressor',
+    link: 'https://ajayprgmr.github.io/Sorting-Visualizer/',
+  },
+  {
+    id: 2,
+    image:
+      'https://user-images.githubusercontent.com/89273037/176277077-27cbc3eb-d116-4e80-81d0-037611eb34c2.gif',
+    project_name: 'Image-Compressor',
+    link: 'https://ajayprgmr.github.io/Sorting-Visualizer/',
+  },
+  {
+    id: 2,
+    image:
+      'https://user-images.githubusercontent.com/89273037/176277077-27cbc3eb-d116-4e80-81d0-037611eb34c2.gif',
+    project_name: 'Image-Compressor',
+    link: 'https://ajayprgmr.github.io/Sorting-Visualizer/',
+  },
+  {
+    id: 2,
+    image:
+      'https://user-images.githubusercontent.com/89273037/176277077-27cbc3eb-d116-4e80-81d0-037611eb34c2.gif',
+    project_name: 'Image-Compressor',
+    link: 'https://ajayprgmr.github.io/Sorting-Visualizer/',
+  },
+  {
+    id: 2,
+    image:
+      'https://user-images.githubusercontent.com/89273037/176277077-27cbc3eb-d116-4e80-81d0-037611eb34c2.gif',
+    project_name: 'Image-Compressor',
+    link: 'https://ajayprgmr.github.io/Sorting-Visualizer/',
+  },
+  {
+    id: 2,
+    image:
+      'https://user-images.githubusercontent.com/89273037/176277077-27cbc3eb-d116-4e80-81d0-037611eb34c2.gif',
+    project_name: 'Image-Compressor',
+    link: 'https://ajayprgmr.github.io/Sorting-Visualizer/',
+  },
+]
+
+const social_media = [
+  {
+    id: 1,
+    name: 'facebook',
+    url: 'https://www.facebook.com/profile.php?id=100005873025208',
+    image: './src/assets/images/facebook.png',
+  },
+  {
+    id: 2,
+    name: 'instagram',
+    url: 'https://www.instagram.com/ajay_prgmr/',
+    image: './src/assets/images/instagram.png',
+  },
+  {
+    id: 3,
+    name: 'twiter',
+    url: 'https://www.instagram.com/ajay_prgmr/',
+    image: './src/assets/images/twitter.png',
+  },
+  {
+    id: 4,
+    name: 'linkedin',
+    url: 'https://www.linkedin.com/in/ajay-kushwaha-4ab438214/',
+    image: './src/assets/images/linkedin.png',
+  },
+  {
+    id: 5,
+    name: 'github',
+    url: 'https://github.com/ajayprgmr',
+    image: './src/assets/images/github.png',
+  },
+]
+const data = {
+  highlighted_works,
+  social_media,
+}
+
+export default data
